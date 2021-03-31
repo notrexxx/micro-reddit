465c509e21b9df6309270a9347ddcdf25499c650
notrexxx <notrexx15@gmail.com>
2021-03-31 18:00:26 -0400

    Add id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_205243) do
+ActiveRecord::Schema.define(version: 2021_03_31_214507) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
