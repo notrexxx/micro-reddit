dcda334b3e304f7f7d0127b4f5171f4c85a8efb0
notrexxx <notrexx15@gmail.com>
2021-03-31 18:51:36 -0400

    Add id keys to comments

---
+++ db/migrate/20210331222945_add_id_to_comments.rb
@@ -0,0 +1,6 @@
+class AddIdToComments < ActiveRecord::Migration[6.1]
+  def change
+    add_column :comments, :user_id, :integer
+    add_column :comments, :post_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_214507) do
+ActiveRecord::Schema.define(version: 2021_03_31_222945) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
