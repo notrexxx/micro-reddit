13fc5365695a5f4cfe33e4cf3f26ae2278049324
notrexxx <notrexx15@gmail.com>
2021-03-31 18:52:15 -0400

    Add models for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
-
+    has_many :comments
+    
     validates :title, presence: true, length: { maximum: 100 }
     validates :content, presence: true, length: { maximum: 500 }
 
